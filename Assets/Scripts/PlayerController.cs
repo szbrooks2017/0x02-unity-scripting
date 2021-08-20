7874c701fddebaad509c5704fd54cc7d7f95efeb
Stratton Brooks <69229545+szbrooks2017@users.noreply.github.com>
2021-08-20 17:55:48 -0500

    update controller again

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
-    public float speed = 1000f;
+    public float speed = 1000;
     private int score = 0;
     // Start is called before the first frame update
     void Start()
@@ -12,10 +12,8 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+     void FixedUpdate()
     {
-        rb.AddForce(0, 0, speed * Time.deltaTime);
-
         if ( Input.GetKey("d"))
         {
             rb.AddForce(speed * Time.deltaTime, 0, 0);
@@ -26,19 +24,20 @@ public class PlayerController : MonoBehaviour
         }
         if ( Input.GetKey("w"))
         {
-            rb.AddForce(0, speed * Time.deltaTime, 0);
+            rb.AddForce(0, 0, speed * Time.deltaTime);
         }
         if ( Input.GetKey("s"))
         {
-            rb.AddForce(0, -speed * Time.deltaTime, 0);
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
         }
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<Collider>().tag == "Pickup");
+        if (other.gameObject.CompareTag("Pickup"));
         {
             score++;
             Debug.Log($"Score: {score}");
+            Destroy(other.gameObject);
         }
     }
 }
